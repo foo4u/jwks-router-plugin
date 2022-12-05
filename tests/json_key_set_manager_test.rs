ca7ef8362e45d8262bc8585886df861568348b8b
Scott Rossillo <scott@rossillo.net>
2022-12-05 10:17:44 -0500

    add startup test

--- tests/json_key_set_manager_test.rs
+++ tests/json_key_set_manager_test.rs
@@ -14,9 +14,10 @@
  * limitations under the License.
 */
 use crate::fixtures::json_web_key_set::{create_jwk_set, create_rsa_key};
-use anyhow::anyhow;
+use anyhow::{anyhow, Error};
 use reqwest::StatusCode;
 use std::time::Duration;
+use jsonwebtoken::jwk::JwkSet;
 use tower::BoxError;
 use wiremock::matchers::{method, path};
 use wiremock::{Mock, MockServer, ResponseTemplate};
@@ -25,6 +26,19 @@ use jwks_router_plugin::jwks_manager;
 
 mod fixtures;
 
+async fn mock_jwks_response(mock_server: &MockServer, key_set: JwkSet, mock_times: u64) {
+     Mock::given(method("GET"))
+        .and(path("/jwks.json"))
+        .respond_with(ResponseTemplate::new(StatusCode::OK).set_body_json(key_set))
+         .up_to_n_times(mock_times)
+        .mount(&mock_server)
+        .await;
+}
+
+fn mock_jwks_uri(mock_server: &MockServer) -> String {
+    format!("{}/jwks.json", mock_server.uri())
+}
+
 #[tokio::test]
 async fn it_retrieves_to_jwks() -> Result<(), BoxError> {
     let kid = "foo";
@@ -32,15 +46,10 @@ async fn it_retrieves_to_jwks() -> Result<(), BoxError> {
     let mock_server = MockServer::start().await;
     let key_set = create_jwk_set(&rsa, kid.to_string());
 
-    Mock::given(method("GET"))
-        .and(path("/jwks.json"))
-        .respond_with(ResponseTemplate::new(StatusCode::OK).set_body_json(key_set))
-        .mount(&mock_server)
-        .await;
+    mock_jwks_response(&mock_server, key_set, 100).await;
 
-    let url = format!("{}/jwks.json", &mock_server.uri());
     let mut mgr =
-        jwks_manager::JwksManager::new(url.as_str(), Some(Duration::from_millis(10))).await?;
+        jwks_manager::JwksManager::new(mock_jwks_uri(&mock_server).as_str(), Some(Duration::from_millis(10))).await?;
     let _ = &mgr.poll();
 
     let result_key_set = &mgr.retrieve_key_set()?;
@@ -67,16 +76,10 @@ async fn it_handles_token_refresh_errors() -> Result<(), BoxError> {
     let mock_server = MockServer::start().await;
     let key_set = create_jwk_set(&rsa, kid.to_string());
 
-    Mock::given(method("GET"))
-        .and(path("/jwks.json"))
-        .respond_with(ResponseTemplate::new(StatusCode::OK).set_body_json(key_set))
-        .up_to_n_times(1)
-        .mount(&mock_server)
-        .await;
+    mock_jwks_response(&mock_server, key_set, 1).await;
 
-    let url = format!("{}/jwks.json", &mock_server.uri());
     let mut mgr =
-        jwks_manager::JwksManager::new(url.as_str(), Some(Duration::from_millis(200))).await?;
+        jwks_manager::JwksManager::new(mock_jwks_uri(&mock_server).as_str(), Some(Duration::from_millis(200))).await?;
     let _ = &mgr.poll();
     let _x = &mgr.retrieve_key_set()?;
 
@@ -97,4 +100,19 @@ async fn it_handles_token_refresh_errors() -> Result<(), BoxError> {
     );
 
     Ok(())
+}
+
+#[tokio::test(flavor = "multi_thread")]
+async fn it_returns_an_error_if_key_set_not_retrievable_on_new() -> Result<(), Error> {
+    let mock_server = MockServer::start().await;
+
+    let mgr =
+        jwks_manager::JwksManager::new(mock_jwks_uri(&mock_server).as_str(), Some(Duration::from_millis(200))).await;
+
+    match mgr {
+        Ok(_) => { Err(anyhow!("Expected manager startup to fail")) },
+        Err(_e) => {
+            Ok(())
+        }
+    }
 }
